39545c5691597c765d7ad0f6772a222d6053ae60
Yuvraj Singh <183982666+0-YuvrajSingh@users.noreply.github.com>
2025-05-29 09:29:08 +0000

    topic 1 done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const userId = 1222;
+let userName = "anything";
+var userPassword = "stylist";
+let userState;
+/* We can reserve the memory
+    for a variable in js
+without giving it a value. */ 
+
+// var had scope problem : In its place let is used.
+
+console.table([userId, userName, userPassword, userState]);
+//userState is undefined beacause no value declared.
