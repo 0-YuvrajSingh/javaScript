110a2077a7edfa9d17d3864e8509367cd9ab3f17
Yuvraj Singh <183982666+0-YuvrajSingh@users.noreply.github.com>
2025-05-29 12:59:11 +0000

    typeConversion

---
+++ 01_basics/03_conversionOperations_02.js
@@ -0,0 +1,41 @@
+// This lecture for Operators and its different uses.
+
+let a = 5;
+let b = 10;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+console.log(a%b);
+
+let str1 = "Master_Control";
+let str2 = " Unlock";
+ let str3 = str1 + str2;
+ console.log(str3);
+ 
+ let as = "1";
+ let bn = 2;
+ let an = 1;
+ let bs = "2";
+
+ 
+ console.log(as + bn);
+ console.log(as + bs);
+ console.log(an + bn + as);
+ console.log(an + bn + bs + as);
+ 
+ /* If any string is present first then output will be string.
+    If string is present at last and previous are numbers 
+    they will be added normally then again concatenate with strings.
+*/
+ 
+console.log(+true); // 1
+//console.log(true+);
+console.log(+"");   // 0
+
+/* Above is also a form of typeConversion but is not recommended. */
+
+
+
+
