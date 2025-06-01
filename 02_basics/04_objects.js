862c79c457ef65911928c6c47d3d73fa0a96d6c4
Yuvraj Singh <183982666+0-YuvrajSingh@users.noreply.github.com>
2025-06-01 10:40:50 +0000

    Acquaintance with Object Properties in JavaScript

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -42,3 +42,39 @@ console.log(ob6);
    {} is an empty object literal — this is the target.
    Properties from obj1 and obj2 are copied into this new empty object.
 */ 
+
+// Another Method :
+const ob8 = {...ob1,...ob2,...ob3};
+console.log(ob8);
+
+// Array of Objects : (generally during fetching)
+const newUserInfo = [
+    {
+        id: 1,
+        email: "starter1@gmail.com"
+    },
+    {
+        id: 2,
+        email: "starter2@gmail.com"
+    },
+    {
+        id: 3,
+        email: "starter3@gmail.com"
+    },
+    {
+        id: 4,
+        email: "starter4@gmail.com"
+    }
+];
+console.log(newUserInfo[0].email); // 0-based indexing
+
+console.log(appUser);
+console.log(Object.keys(appUser));
+console.log(Object.values(appUser));
+console.log(Object.entries(appUser));
+console.log(appUser.hasOwnProperty('isLoggedIn'));
+console.log(typeof Object.keys(appUser));
+// It shows object and array are a special type of an object.
+// Correct way to check if it is an array:
+console.log(Array.isArray(Object.keys(appUser)));
+//true;
